222b48b50f630ae1a360d3d91d73b1f535dbd2db
Heyang-Li <heyang.li@example.com>
2026-08-02 14:09:34 +0800

    remove debug scripts

--- cards/林清弦/debug_ext.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('/sessions/cool-stoic-gates/mnt/林清弦/林清弦_fixed.json','r') as f:
-    data = json.load(f)
-
-ext = data['data']['extensions']
-print(json.dumps(ext, indent=2, ensure_ascii=False)[:4000])
